ebe839f5c9d64bae80e1d6a15e41aeb9072f8b2d
Joe Winkler <joe.c.winkler@outlook.com>
2018-11-07 15:06:17 -0500

    Added logging and request parsing

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@
 
 import {config} from './config/index';
 import mongoose from 'mongoose';
-import express from 'express';
+import Express from 'express';
+import bodyParser from 'body-parser';
+import logger from 'morgan';
 
 console.log(`You are running in a ${config.server.env} environment.`);
 
@@ -12,16 +14,23 @@ const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 // Initialize express app and router
-const app = new express();
-const router = new express.Router();
+const app = new Express();
+const router = new Express.Router();
+
+// Use x-www-form-urlencoded and json parsing
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+// Use morgan to log incoming requests
+app.use(logger('dev'));
 
 // Sample API endpoint
 router.route('/*')
-  .get((req, res) => {
-    res.json({
-      message: 'Hello World!',
+    .get((req, res) => {
+      res.json({
+        message: 'Hello World!',
+      });
     });
-  });
 
 app.use('/api', router);
 
